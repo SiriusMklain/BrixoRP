4d9f95cb2bf1b650b866f03ba0c43b7b181b7b7c
SiriusMklain <sergeimakarenko5991@gmail.com>
2023-11-22 20:00:30 +0300

    sdfsd

--- app2/admin.py
+++ app2/admin.py
@@ -1,3 +1,134 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import UserProfile, VisitToTT, TextSlide, ImageSlide
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'workplace')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    def has_change_permission(self, request, obj=None):
+        if not obj:  # Добавление новой записи
+            return True
+        # Редактирование существующей записи
+        return obj.user == request.user or request.user.is_superuser
+
+    def get_model_perms(self, request):
+        """
+        Получение разрешений для модели.
+        """
+        # Для суперпользователя отображаем все
+        if request.user.is_superuser:
+            return super().get_model_perms(request)
+
+        # Для обычных пользователей скрываем
+        return {}
+
+
+@admin.register(VisitToTT)
+class VisitToTTAdmin(ModelAdmin):
+    list_display = [
+        'user_profile',
+        'visits',
+        'retail',
+        'sto',
+        'retail_buy_SAKURA_and_have',
+        'sto_buy_SAKURA_and_have',
+        'sto_dont_buy_SAKURA_and_have',
+        'retail_dont_buy_SAKURA_and_have',
+        'month'
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user_profile:
+            obj.user_profile = UserProfile.objects.get(user=request.user)
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user_profile__user=request.user)
+
+    def has_change_permission(self, request, obj=None):
+        if not obj:  # Добавление новой записи
+            return True
+        # Редактирование существующей записи
+        return obj.user_profile.user == request.user or request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        if not obj:  # Удаление новой записи
+            return True
+        # Удаление существующей записи
+        return obj.user_profile.user == request.user or request.user.is_superuser
+
+
+@admin.register(TextSlide)
+class TextSlideAdmin(ModelAdmin):
+    list_display = [
+        'visit',
+        'user_profile',
+        'title',
+        'text'
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user_profile:
+            obj.user_profile = UserProfile.objects.get(user=request.user)
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user_profile__user=request.user)
+
+    def has_change_permission(self, request, obj=None):
+        if not obj:  # Добавление новой записи
+            return True
+        # Редактирование существующей записи
+        return obj.user_profile.user == request.user or request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        if not obj:  # Удаление новой записи
+            return True
+        # Удаление существующей записи
+        return obj.user_profile.user == request.user or request.user.is_superuser
+
+
+@admin.register(ImageSlide)
+class ImageSlideAdmin(ModelAdmin):
+    list_display = [
+        'visit',
+        'user_profile',
+        'title'
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user_profile:
+            obj.user_profile = UserProfile.objects.get(user=request.user)
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user_profile__user=request.user)
+
+    def has_change_permission(self, request, obj=None):
+        if not obj:  # Добавление новой записи
+            return True
+        # Редактирование существующей записи
+        return obj.user_profile.user == request.user or request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        if not obj:  # Удаление новой записи
+            return True
+        # Удаление существующей записи
+        return obj.user_profile.user == request.user or request.user.is_superuser
--- app2/models.py
+++ app2/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """Пользователь"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    workplace = models.CharField(max_length=255, verbose_name="Область работы")
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+
+
+class VisitToTT(models.Model):
+    """Посещение ТТ:"""
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, editable=False, null=True)
+    visits = models.CharField(max_length=255, verbose_name='Визитов (ТТ)')
+    retail = models.CharField(max_length=255, verbose_name='Розничных ТТ')
+    sto = models.CharField(max_length=255, verbose_name='СТО')
+    retail_buy_SAKURA_and_have = models.CharField(max_length=255, verbose_name='Закупают SAKURA и есть в наличии')
+    sto_buy_SAKURA_and_have = models.CharField(max_length=255, verbose_name='Закупают SAKURA и есть в наличии')
+    sto_dont_buy_SAKURA_and_have = models.CharField(max_length=255, verbose_name='СТО Не закупают SAKURA или только под заказ в %')
+    retail_dont_buy_SAKURA_and_have = models.CharField(max_length=255, verbose_name='Розничные ТТ Не закупают SAKURA или только под заказ в %')
+    month = models.DateField(auto_now=False, auto_now_add=False, verbose_name='За какой месяц?')
+
+    def save(self, *args, **kwargs):
+        # Подставляем текущего пользователя в поле user_profile перед сохранением
+        if not self.user_profile:
+            # Если поле user_profile не заполнено
+            user_profile, created = UserProfile.objects.get_or_create(user=self.user)
+            self.user_profile = user_profile
+        super(VisitToTT, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.user_profile} {self.month}'
+
+    class Meta:
+        verbose_name = 'Посещение ТТ:'
+        verbose_name_plural = 'Посещение ТТ:'
+
+
+class TextSlide(models.Model):
+    """Текстовый Слайдер"""
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, editable=False, null=True)
+    visit = models.ForeignKey(VisitToTT, on_delete=models.CASCADE, verbose_name='Посещение ТТ:')
+    title = models.CharField(max_length=255, verbose_name='Заголовок листа')
+    text = models.TextField(verbose_name="Текстовое поле")
+
+    def save(self, *args, **kwargs):
+        # Подставляем текущего пользователя в поле user_profile перед сохранением
+        if not self.user_profile:
+            # Если поле user_profile не заполнено
+            user_profile, created = UserProfile.objects.get_or_create(user=self.user)
+            self.user_profile = user_profile
+        super(TextSlide, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Текстовый Слайдер'
+        verbose_name_plural = 'Текстовые Слайдеры'
+
+
+class ImageSlide(models.Model):
+    """Image Слайдер"""
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, editable=False, null=True)
+    visit = models.ForeignKey(VisitToTT, on_delete=models.CASCADE, verbose_name='Посещение ТТ:')
+    title = models.CharField(max_length=255, verbose_name='Заголовок листа')
+    image = models.ImageField(upload_to='image_slide/')
+
+    def save(self, *args, **kwargs):
+        # Подставляем текущего пользователя в поле user_profile перед сохранением
+        if not self.user_profile:
+            # Если поле user_profile не заполнено
+            user_profile, created = UserProfile.objects.get_or_create(user=self.user)
+            self.user_profile = user_profile
+        super(ImageSlide, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Image Слайдер'
+        verbose_name_plural = 'Image Слайдеры'
